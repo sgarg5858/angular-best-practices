699c23591d1c230155ef3e7c6530a295703caeb5
Sanjay Garg <sgarg5858@gmail.com>
2022-08-16 13:51:09 +0530

    canActivateImplemented

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { IsUserLoggedInService } from './auth/is-user-logged-in.service';
+import { LogInComponent } from './auth/log-in/log-in.component';
+import { SignUpComponent } from './auth/sign-up/sign-up.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'signup',component:SignUpComponent,canActivate:[IsUserLoggedInService]
+  },
+  {
+    path:'login',component:LogInComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/auth/is-user-logged-in.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { delay, map, Observable, tap } from 'rxjs';
+import { AuthService,User } from './auth.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class IsUserLoggedInService implements CanActivate {
+
+  constructor(private authService:AuthService,private router:Router) { }
+
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | UrlTree | Observable<boolean | UrlTree> | Promise<boolean | UrlTree> {
+
+    return this.authService.user$.pipe(
+      delay(2000),
+      map((user)=>user? true : false),
+      tap((loggedIn:boolean)=>{
+        if(!loggedIn)
+        {
+          //check this if it works?
+          this.router.navigate(['login'],{queryParams:{returnUrl:route.url}});
+        }
+      })
+    )
+  }
+}
